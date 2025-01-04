5ee42e5a37f411c23bfe6a5051dfe9b75f8786be
Mahmoud Atif <mahmoudateff07@gmail.com>
2025-01-04 14:49:20 +0200

    Add file model

---
+++ core/files/models/file_model.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import FileExtensionValidator
+
+
+class FileModel(models.Model):
+    file = models.FileField(
+        _("File"),
+        upload_to="uploads/",
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["pdf", "jpg", "jpeg", "png"]
+            )
+        ],
+    )
+    extention = models.CharField(_("Extention"), max_length=10)
+    width = models.FloatField(_("Width"), null=True, blank=True)
+    height = models.FloatField(_("Height"), null=True, blank=True)
+    number_of_channels = models.FloatField(
+        _("Number of channels"), null=True, blank=True
+    )
+    number_of_pages = models.FloatField(
+        _("Number of pages"), null=True, blank=True
+    )
+
+    class Meta:
+        db_table = "files_filemodel"
+        verbose_name = _("File Model")
+        verbose_name_plural = _("Files Model")
